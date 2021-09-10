6ee3934bd24d2c8ed63ee93d9c30be8d055915f6
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-10 19:37:11 -0400

    Add foreign keys to the database schema in the role table and employee table

--- db/schema.sql
+++ db/schema.sql
@@ -7,17 +7,23 @@ CREATE TABLE IF NOT EXISTS department (
       name VARCHAR(30) NOT NULL,
       );
 
+DROP TABLE IF EXISTS role
 CREATE TABLE IF NOT EXISTS role (
       id INT PRIMARY KEY,
       title VARCHAR(30) NOT NULL,
       salary DECIMAL NOT NULL,
-      department_id INT NOT NULL
+      department_id INT NOT NULL,
+      FOREIGN KEY (department_id) 
+      REFERENCES department(id)
       );
 
-CREATE TABLE IF NOT EXISTS role (
+DROP TABLE IF EXISTS role;
+CREATE TABLE IF NOT EXISTS employee (
       id INT PRIMARY KEY,
       first_name VARCHAR(30) NOT NULL,
       last_name VARCHAR(30) NOT NULL,
       role_id DECIMAL NOT NULL,
       manager_id INT NULL
+      FOREIGN KEY (role_id)
+      REFERENCES role(id)
       );
