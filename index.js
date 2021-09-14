335c93c126b74cd114e95e2b6476c58e9506fd7b
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-14 17:36:24 -0400

    Remove manager_id from table of employee data during addEmployee(), begin creating inquirer prompts to add an employee. Create the functionality to incorporate the roles inside the role table as choices in the inquirer list prompt to select a role for the new employee

--- index.js
+++ index.js
@@ -239,12 +239,44 @@ function addEmployee() {
                               result[i].manager = result[result[i].manager_id - 1].first_name + " " + result[result[i].manager_id - 1].last_name;
                         }
             }
-            console.log(result);
-      })
+            delete result[i].manager_id;
+            console.table(result);
+
+            let managers = result;
+
+            const empPrompts = [
+                  {
+                        type: 'input',
+                        name: 'first_name',
+                        message: 'What is the first name of the employee?'
+                  },
+                  {
+                        type: 'input',
+                        name: 'last_name',
+                        message: 'What is the last name of the employee?'
+                  },
+                  {
+                        type: 'list',
+                        name: 'role',
+                        message: 'What is the role of the new employee?',
+                        choices: function(){
+                              let roleList = [];
+
+                              for(i=0; i < roles.length; i++){
+                                    roleList.push(roles[i].title);
+                              }
+                              return roleList;
+                        }
+                  }
+            ]
+
+            inquire.prompt(empPrompts)
+            .then((answers) =>{
+                  console.table(answers);
+            })
 
       })
-
-}
+})}
 
 function updateEmployee() {}
 
