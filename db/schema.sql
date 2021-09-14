bc2ddb0b8de5a962d251fa2a9ac253e8e1c5c1de
Luke Poirrier <lukepoirrier5@gmail.com>
2021-09-14 14:10:50 -0400

    Schema bugs worked out. Now set up with foreign keys to connect the tables.

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,35 @@
-DROP DATABASE IF EXISTS employeeTracker;
-CREATE DATABASE employeeTracker;
-USE employeeTracker;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
-CREATE TABLE IF NOT EXISTS department (
-      id INT PRIMARY KEY,
-      name VARCHAR(30) NOT NULL,
-      );
-
-DROP TABLE IF EXISTS role
-CREATE TABLE IF NOT EXISTS role (
-      id INT PRIMARY KEY,
-      title VARCHAR(30) NOT NULL,
-      salary DECIMAL NOT NULL,
-      department_id INT NOT NULL,
-      FOREIGN KEY (department_id) 
-      REFERENCES department(id)
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+      id INT NOT NULL AUTO_INCREMENT,
+      department_name VARCHAR(30),
+      PRIMARY KEY(id)
       );
 
 DROP TABLE IF EXISTS role;
-CREATE TABLE IF NOT EXISTS employee (
+
+CREATE TABLE role (
+      role_id INT NOT NULL,
+      title VARCHAR(30),
+      salary DECIMAL,
+      department_id INT NULL,
+      FOREIGN KEY (department_id)
+      REFERENCES department(id) 
+      ON DELETE SET NULL,
+      PRIMARY KEY(role_id)
+      );
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
       id INT PRIMARY KEY,
       first_name VARCHAR(30) NOT NULL,
       last_name VARCHAR(30) NOT NULL,
-      role_id DECIMAL NOT NULL,
-      manager_id INT NULL
+      role_id INT NULL,
+      manager_id INT NULL,
       FOREIGN KEY (role_id)
-      REFERENCES role(id)
+      REFERENCES role(role_id)
+      ON DELETE SET NULL
       );
